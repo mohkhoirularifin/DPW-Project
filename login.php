c4b18890e3227975f99507f42e536323733cf95b
Moh. Khoirul Arifin <karifin998@gmail.com>
2018-12-04 09:03:53 +0700

    Pembuatan File Login

---
+++ login.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <title>Desain Form Login Dengan Css</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="konten">
+        <div class="kepala">
+            <div class="lock"></div>
+            <h2 class="judul">Log In</h2>
+        </div>
+        <div class="artikel">
+            <form action="#" method="post">
+                <div class="grup">
+                    <label for="username">Username</label>
+                    <input type="text" placeholder="Username">
+                </div>
+                <div class="grup">
+                    <label for="password">Password</label>
+                    <input type="password" placeholder="Password">
+                </div>
+                    <input type="checkbox" name="remember" id="remember">
+                    <label for="remember">Remember Me</label>
+                <div class="grup">
+                    <input type="submit" value="Log In">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
