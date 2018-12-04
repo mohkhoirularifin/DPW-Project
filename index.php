6f3888207d574951ba041d112f52da3251227544
Moh. Khoirul Arifin <karifin998@gmail.com>
2018-12-04 10:17:58 +0700

    Penambahan Navbar dan Tampilan awal web

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+    
+    <nav class="navbar navbar-default" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Link</a>
+        </div>
+    
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Database</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+            <form class="navbar-form navbar-right" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Cari">
+                </div>
+                <button type="submit" class="btn btn-default">Cari</button>
+            </form>
+        </div><!-- /.navbar-collapse -->
+    </nav>
+    
+    <div class="wrapper">
+        <div class="sidebar"><h1>Sidebar</h1></div>
+        <div class="content"><h1>Content</h1></div>
+    </div>
+</body>
+</html>
