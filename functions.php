6f7d472523e7bb61797834bd7debf88be39dcd53
Moh. Khoirul Arifin <karifin998@gmail.com>
2018-12-22 06:42:58 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -45,6 +45,7 @@ function registrasi($data)
     global $conn;
 
     $username=strtolower(stripcslashes($data['username']));
+
     $password=mysqli_real_escape_string($conn, $data['password']);
     $password2=mysqli_real_escape_string($conn, $data['password2']);
 
