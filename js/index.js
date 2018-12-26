2daa0e790a4326cca8f252dfe3a787e448ec41d0
Moh. Khoirul Arifin <karifin998@gmail.com>
2018-12-27 01:56:17 +0700

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// CSS Styling by me
+
+// Dropdown menu code from:
+// http://www.gethifi.com/blog/nicer-navigation-with-css-transitions
+
+
+//Allow clicking instead of hover for the dropdown menu
+$(document).ready( function(){
+
+    $('#submenu').click( function(event){
+        event.stopPropagation();
+        $('#drop').toggle();
+    });
+    
+    $(document).click( function(){
+        $('#drop').hide();
+    });
+  
+});
